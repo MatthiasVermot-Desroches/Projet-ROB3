3b8a97054748a7b0276a9bfdc0da33908bb37696
Matthias Vermot - Desroches <115169567+MatthiasVermot-Desroches@users.noreply.github.com>
2023-05-08 21:35:01 +0200

    Create rosmaster.sh

---
+++ ROS/mapping/rosmaster.sh
@@ -0,0 +1,5 @@
+#Ce script vous permet de lancer le rosmaster
+source /home/<user>/catkin_ws/devel/setup.bash
+sudo chmod /dev/ttyUSB0 # les lignes 1 et 2 sont là pour éviter les problèmes en cas d'initialisation du robot
+cd /home/<user>/catkin_ws/
+roslaunch rplidar_ros rplidar.launch #le rosmaster est lancé et il enregistre des points à l'aide du lidar.

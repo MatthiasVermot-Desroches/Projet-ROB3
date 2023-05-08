3ce447d490a0a528e41af3e8abd5026556a7025b
Matthias Vermot - Desroches <115169567+MatthiasVermot-Desroches@users.noreply.github.com>
2023-05-08 21:36:57 +0200

    Update rosmaster.sh

--- ROS/mapping/rosmaster.sh
+++ ROS/mapping/rosmaster.sh
@@ -1,4 +1,5 @@
 #Ce script vous permet de lancer le rosmaster
+#!/bin/bash
 source /home/<user>/catkin_ws/devel/setup.bash
 sudo chmod /dev/ttyUSB0 # les lignes 1 et 2 sont là pour éviter les problèmes en cas d'initialisation du robot
 cd /home/<user>/catkin_ws/

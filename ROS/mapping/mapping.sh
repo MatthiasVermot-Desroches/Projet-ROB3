abd6e7457f67a39fbb5669396d4ee5214d605235
Matthias Vermot - Desroches <115169567+MatthiasVermot-Desroches@users.noreply.github.com>
2023-05-08 21:36:39 +0200

    Create mapping.sh

---
+++ ROS/mapping/mapping.sh
@@ -0,0 +1,2 @@
+#Ce script permet de lancer la cartographie, si le robot est branché à une interface graphique, vous pourrez la voir se créer et modifier les paramètres et l'angle de vue en direct.
+#!/bin/bash

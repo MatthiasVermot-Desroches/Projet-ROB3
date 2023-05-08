9d5c6a5e1a953fd5131bf3a32511c6064f534ea9
Matthias Vermot - Desroches <115169567+MatthiasVermot-Desroches@users.noreply.github.com>
2023-05-08 21:40:23 +0200

    Update mapping.sh

--- ROS/mapping/mapping.sh
+++ ROS/mapping/mapping.sh
@@ -1,2 +1,5 @@
 #Ce script permet de lancer la cartographie, si le robot est branché à une interface graphique, vous pourrez la voir se créer et modifier les paramètres et l'angle de vue en direct.
 #!/bin/bash
+source /home/<user>/catkin_ws/devel/setup.bash #cette ligne est là pour éviter les problèmes liés à l'initialisation du robot
+cd /home/<user>/catkin_ws/
+roslaunch hector_slam_launch tutorial.launch #l'algorithme slam et une carte vide est lancée dans l'interface du robot, la carte est mise à jours avec les points créer par le Lidar.
